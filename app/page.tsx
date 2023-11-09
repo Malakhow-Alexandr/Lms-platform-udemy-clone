740ae24e4482af76dbbcb353222bb3cc5b7ed67a
Malakhow-Alexandr <chuk3pc@gmail.com>
2023-11-09 18:12:38 +0200

    add clerk auth and sign-in sign-up pages

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main>
-      <div className="h-[40px] bg-red-700 w-[100px]">Hello world</div>
-    </main>
-  );
-}
