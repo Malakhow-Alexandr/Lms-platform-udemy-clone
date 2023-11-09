a145bb061c602ec691c6ad3b4369d0482b6d26a6
Malakhow-Alexandr <chuk3pc@gmail.com>
2023-11-09 17:00:22 +0200

    add start files

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="h-[40px] bg-red-700 w-[100px]">Hello world</div>
+    </main>
+  );
+}
