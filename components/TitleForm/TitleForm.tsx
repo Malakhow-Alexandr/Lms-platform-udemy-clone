a1c1f60aa2bb7ba2e550c511c7891c5f5d932a49
Malakhow-Alexandr <chuk3pc@gmail.com>
2023-11-13 18:10:54 +0200

    add Title FormComponent

---
+++ components/TitleForm/TitleForm.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface TitleFormProps {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+});
+
+const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  return <div>TitleForm</div>;
+};
+
+export default TitleForm;
